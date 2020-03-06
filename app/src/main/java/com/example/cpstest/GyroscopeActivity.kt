517e0a70c7ce5c0bbb48f48c5f02337621cd6e72
zahramo <moosavizahra67@yahoo.com>
2020-03-07 00:07:31 +0330

    Add GravityActivity and GyroscopeActivity

---
+++ app/src/main/java/com/example/cpstest/GyroscopeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cpstest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GyroscopeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gyroscope)
+    }
+}
--- app/src/main/java/com/example/cpstest/MainActivity.java
+++ app/src/main/java/com/example/cpstest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cpstest;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -7,6 +8,7 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,19 +22,37 @@ import static android.hardware.Sensor.TYPE_GYROSCOPE;
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor sensor;
-    private TextView tv;
-    private Paint paint = new Paint();
+    Button gyroscopeBtn;
+    Button gravityBtn;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        System.out.println("hello i'm hereeeee");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gyroscopeBtn = (Button)findViewById(R.id.gyroscopeBtn);
+        gravityBtn = (Button)findViewById(R.id.gravityBtn);
+
+        gravityBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent gravityActivity = new Intent(getBaseContext(), GravityActivity.class);
+                startActivity(gravityActivity);
+            }
+        });
+
+        gyroscopeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent gyroscopeActivity = new Intent(getBaseContext(), GyroscopeActivity.class);
+                startActivity(gyroscopeActivity);
+            }
+        });
+
 
 //        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 //        sensor = sensorManager.getDefaultSensor(TYPE_GYROSCOPE);
 //        tv = findViewById(R.id.tv1);
-
-        //Button btn = (Button)findViewById(R.id.gyroscopeBtn);
 
     }
 
