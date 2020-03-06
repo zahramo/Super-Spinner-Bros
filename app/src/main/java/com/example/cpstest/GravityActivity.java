d02495a1ddef1d6281781bae3084c290c4c55a8e
kimia khabiri <kimia.khabiri.92@gmail.com>
2020-03-07 02:29:44 +0330

    Replace .kt Activities with .java

---
+++ app/src/main/java/com/example/cpstest/GravityActivity.java
@@ -0,0 +1,13 @@
+package com.example.cpstest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GravityActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gravity);
+    }
+}
