67bb4ac0d6a6af89e831f7db637eab9313a1bbf3
zahramo <moosavizahra67@yahoo.com>
2020-03-06 20:58:33 +0330

    Add View of First Page

--- app/src/main/java/com/example/cpstest/MainActivity.java
+++ app/src/main/java/com/example/cpstest/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.cpstest;
 
+import android.graphics.Paint;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -19,34 +21,37 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor sensor;
     private TextView tv;
+    private Paint paint = new Paint();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         System.out.println("hello i'm hereeeee");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(TYPE_GYROSCOPE);
-        tv = findViewById(R.id.tv1);
+//        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+//        sensor = sensorManager.getDefaultSensor(TYPE_GYROSCOPE);
+//        tv = findViewById(R.id.tv1);
 
+        //Button btn = (Button)findViewById(R.id.gyroscopeBtn);
 
     }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+       // sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        sensorManager.unregisterListener(this);
+//        sensorManager.unregisterListener(this);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        tv.setText("X:" + event.values[0] + ", Y:" + event.values[1] + ", Z:" + event.values[2]);
+        //tv.setText("X:" + event.values[0] + ", Y:" + event.values[1] + ", Z:" + event.values[2]);
     }
 
     @Override
