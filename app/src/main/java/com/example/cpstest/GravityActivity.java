fe81d538743944f438486411b926142a315ac925
zahramo <moosavizahra67@yahoo.com>
2020-03-07 18:12:58 +0330

    Add x,y,speed Input and Merge

--- app/src/main/java/com/example/cpstest/GravityActivity.java
+++ app/src/main/java/com/example/cpstest/GravityActivity.java
@@ -1,13 +1,40 @@
 package com.example.cpstest;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GravityActivity extends AppCompatActivity {
+    private int ball1X;
+    private int ball2X;
+    private int ball1Y;
+    private int ball2Y;
+    private int ball1Speed;
+    private int ball2Speed;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravity);
+        setBallsData();
     }
+
+    private void setBallsData(){
+        ball1X = Integer.parseInt(getIntent().getStringExtra("ball1X"));
+        System.out.println(ball1X);
+        ball1Y = Integer.parseInt(getIntent().getStringExtra("ball1Y"));
+        System.out.println(ball1Y);
+        ball1Speed = Integer.parseInt(getIntent().getStringExtra("ball1Speed"));
+        System.out.println(ball1Speed);
+
+        ball2X = Integer.parseInt(getIntent().getStringExtra("ball2X"));
+        System.out.println(ball2X);
+        ball2Y = Integer.parseInt(getIntent().getStringExtra("ball2Y"));
+        System.out.println(ball2Y);
+        ball2Speed = Integer.parseInt(getIntent().getStringExtra("ball2Speed"));
+        System.out.println(ball2Speed);
+    }
+
+
 }
--- app/src/main/java/com/example/cpstest/MainActivity.java
+++ app/src/main/java/com/example/cpstest/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     Button gyroscopeBtn;
     Button gravityBtn;
 
+    private EditText ball1X;
+    private EditText ball2X;
+    private EditText ball1Y;
+    private EditText ball2Y;
+    private EditText ball1Speed;
+    private EditText ball2Speed;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,10 +41,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         gyroscopeBtn = (Button)findViewById(R.id.gyroscopeBtn);
         gravityBtn = (Button)findViewById(R.id.gravityBtn);
 
+        ball1X = (EditText) findViewById(R.id.ball1X);
+        ball1Y = (EditText) findViewById(R.id.ball1Y);
+        ball1Speed = (EditText) findViewById(R.id.ball1Speed);
+        ball2X = (EditText) findViewById(R.id.ball2X);
+        ball2Y = (EditText) findViewById(R.id.ball2Y);
+        ball2Speed = (EditText) findViewById(R.id.ball2Speed);
+
+
         gravityBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent gravityActivity = new Intent(getBaseContext(), GravityActivity.class);
+                gravityActivity = setInputDatas(gravityActivity);
                 startActivity(gravityActivity);
             }
         });
@@ -77,6 +94,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    private Intent setInputDatas(Intent intent){
+        intent.putExtra("ball1X", ball1X.getText().toString());
+        intent.putExtra("ball2X", ball2X.getText().toString());
+        intent.putExtra("ball1Y", ball1Y.getText().toString());
+
+        intent.putExtra("ball2Y", ball2Y.getText().toString());
+        intent.putExtra("ball1Speed", ball1Speed.getText().toString());
+        intent.putExtra("ball2Speed", ball2Speed.getText().toString());
+        return intent;
     }
 }
 
